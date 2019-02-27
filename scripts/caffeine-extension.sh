467d17735f037290756ddb42fdd630a18a2b260e
Paul Ridgway <paul@ridgway.io>
2019-02-27 09:33:13 +0000

    tidy up

--- scripts/caffeine-extension.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-gnome-shell-extension-tool -e caffeine@patapon.info
-cd /tmp
-git clone git://github.com/eonpatapon/gnome-shell-extension-caffeine.git
-cd gnome-shell-extension-caffeine
-./update-locale.sh
-glib-compile-schemas --strict --targetdir=caffeine@patapon.info/schemas/ caffeine@patapon.info/schemas
-cp -r caffeine@patapon.info ~/.local/share/gnome-shell/extensions
